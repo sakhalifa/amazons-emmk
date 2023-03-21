caaa076a928b61b69edf6338a8bcc454e5039eb0
CHANCRIN Youri <Youri.Chancrin@bordeaux-inp.fr>
2023-03-21 17:29:20 +0100

    Added board struct and neighbors struct

--- src/common/board.h
+++ src/common/board.h
@@ -1,2 +1,18 @@
-#include "graph.h"
+#ifndef _BOARD_H_
+#define _BOARD_H_
 
+#include <stdbool.h>
+
+#include "graph.h"
+#include "player.h"
+#include "move.h"
+#include "dir.h"
+
+typedef struct {
+    struct graph_t graph;
+    unsigned int *queens[NUM_PLAYERS];
+    unsigned int num_queens;
+} board_t;
+
+
+#endif
