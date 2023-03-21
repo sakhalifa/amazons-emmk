64b567a3d77234fec8e91c294884bca1fadb5ba6
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-03-21 17:36:13 +0100

    added some more skeleton

--- src/server/server.c
+++ src/server/server.c
@@ -1,6 +1,19 @@
+#include <stdio.h>
+#include <getopt.h>
 #include "server.h"
 
-int main(int argc, char const *argv[])
+#define USAGE_STRING "Usage: %s [-t |] [-m |] <player1.so> <player2.so>\n"
+
+void print_usage(char *binary_name){
+	printf(USAGE_STRING, binary_name);
+}
+
+server_settings_t get_args(int argc, char *const *argv){
+	
+}
+
+int main(int argc, char *const *argv)
 {
+	
 	return 0;
 }
--- src/server/server.h
+++ src/server/server.h
@@ -3,9 +3,16 @@
 #include <stddef.h>
 #include "player_handle.h"
 
+typedef enum {
+	SQUARE,
+	DONUT,
+	CLOVER,
+	EIGHT
+} game_type_t;
+
 typedef struct {
 	size_t game_width;
-	char game_type;
+	game_type_t game_type;
 	player_handle_t player_handles[NUM_PLAYERS];
 } server_settings_t;
 
