d461da649bed463261a3c2f840259d59d9edfe1f
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-03-21 16:54:38 +0100

    some more skeleton work

--- src/server/player_handle.h
+++ src/server/player_handle.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "player.h"
 
 typedef struct {
--- src/server/server.h
+++ src/server/server.h
@@ -1,12 +1,12 @@
 #ifndef SERVER_H
 #define SERVER_H
 #include <stddef.h>
-#include "player.h"
+#include "player_handle.h"
 
 typedef struct {
 	size_t game_width;
 	char game_type;
-	void *player_handles[NUM_PLAYERS];
+	player_handle_t player_handles[NUM_PLAYERS];
 } server_settings_t;
 
 #endif
