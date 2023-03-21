be37fb130017269e6f4730f1ce28a8af35e9c2f5
CHANCRIN Youri <Youri.Chancrin@bordeaux-inp.fr>
2023-03-21 18:08:38 +0100

    Added skeleton of init game and players function

--- src/server/server.c
+++ src/server/server.c
@@ -88,6 +88,32 @@ void init_game_and_players(server_settings_t settings){
 
 }
 
+
+unsigned int get_starting_player_id(){
+	srand(time(NULL));
+	return rand() % NUM_PLAYERS;
+}
+
+unsigned int get_other_player_id(unsigned int player_id){
+	return player_id == 1 ? 0 : 1;
+}
+
+// TODO : define him better
+void init_game_and_players(server_settings_t settings){
+
+	struct graph_t graph;
+	unsigned int num_queens = 4 * (settings.game_width / 10 + 1);
+	unsigned int* queens[NUM_PLAYERS];
+
+	unsigned int starting_player_id = get_starting_player_id();
+	settings.player_handles[starting_player_id]
+		.initialize(starting_player_id, graph, num_queens, queens);
+	unsigned int other_player_id = get_other_player_id()
+	settings.player_handles[starting_player_id]
+		.initialize(other_player_id, graph, num_queens, queens);
+
+}
+
 int main(int argc, char *const *argv)
 {
 	server_settings_t args = get_args(argc, argv);
