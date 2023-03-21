ee942e2c40270f6f483caa6c2cf3882a62a313c9
DESCOMPS Théo <tdescomps@bordeaux-inp.fr>
2023-03-21 17:33:14 +0100

    player.c wip

--- src/client/player.c
+++ src/client/player.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 #include "player.h"
 
+typedef struct {
+    unsigned int player_id;
+    char name[64];
+    board_t board;
+} player_t;
+
+static player_t player;
+
+void initialize(unsigned int player_id, struct graph_t* graph,
+                unsigned int num_queens, unsigned int* queens[NUM_PLAYERS]) {
+    player.player_id = player_id;
+    player. 
+}
