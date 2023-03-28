2bf51953efd327c8f14b971c8ead2a368b3fcd2c
CHANCRIN Youri <Youri.Chancrin@bordeaux-inp.fr>
2023-03-28 15:56:01 +0200

    Fixed missing curly brace

--- src/server/server.c
+++ src/server/server.c
@@ -64,6 +64,21 @@ server_settings_t get_args(int argc, char *const *argv)
 	return settings;
 }
 
+unsigned int get_starting_player_id()
+{
+	srand(time(NULL));
+	return rand() % NUM_PLAYERS;
+}
+
+unsigned int get_other_player_id(unsigned int player_id)
+{
+	return player_id == 1 ? 0 : 1;
+}
+
+struct graph_t *init_square_graph(size_t width){
+	
+}
+
 
 unsigned int** init_queens(unsigned int num_queens, size_t width){
 	unsigned int** queens = (unsigned int **) malloc(num_queens * NUM_PLAYERS * sizeof(unsigned int));
@@ -129,3 +144,4 @@ int main(int argc, char *const *argv)
 		dlclose(args.player_handles[i].dl_handle);
 	return 0;
 }
+
