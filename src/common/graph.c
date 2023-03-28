405b532ba1b33422f7e83b956862043a3a64655b
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-03-28 15:13:20 +0200

    skeleton and basic function

---
+++ src/common/graph.c
@@ -0,0 +1,12 @@
+#include "graph.h"
+#include "graph_ext.h"
+
+struct graph_t *copy_graph(struct graph_t *source){
+	struct graph_t *cpy = malloc(sizeof(struct graph_t));
+	cpy->num_vertices = source->num_vertices;
+	gsl_spmatrix_uint* tmp = gsl_spmatrix_uint_alloc(source->t->size1, source->t->size2);
+	cpy->t = gsl_spmatrix_uint_compress(tmp, GSL_SPMATRIX_CSR);
+	gsl_spmatrix_uint_free(tmp);
+	gsl_spmatrix_uint_memcpy(cpy->t, source->t);
+	return cpy;
+}
