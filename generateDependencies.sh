e7d0183afe76023408d01bf4512d9b035bf0622c
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-04-04 16:16:11 +0200

    fix clrf???

--- generateDependencies.sh
+++ generateDependencies.sh
@@ -1,6 +1,7 @@
-SOURCEDIR="src"
-CLIENTDIR="$SOURCEDIR/client"
-SERVERDIR="$SOURCEDIR/server"
-COMMONDIR="$SOURCEDIR/common"
+#!/bin/bash
+SOURCEDIR=src
+CLIENTDIR=$SOURCEDIR/client
+SERVERDIR=$SOURCEDIR/server
+COMMONDIR=$SOURCEDIR/common
 
-find . -name *.c | xargs gcc -MM -I$SERVERDIR -I$CLIENTDIR -I$COMMONDIR | sed -E 's/[^\\]$/&\n	gcc -c -I${COMMONDIR} -I${CLIENTDIR} $(CFLAGS) -o $@ $</g' > Makefile.inc
+find . -name *.c | xargs gcc -MM -I$SERVERDIR -I$CLIENTDIR -I$COMMONDIR | sed -E 's/[^\\]$/&\n	gcc -c -I${COMMONDIR} -I${CLIENTDIR} -I${SERVERDIR} $(CFLAGS) -o $@ $</g' > Makefile.inc
