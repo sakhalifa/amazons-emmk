493bf33bdfb1960d1d74eb32858c7b6bae27fa82
DESCOMPS Théo <tdescomps@bordeaux-inp.fr>
2023-04-04 14:46:35 +0200

    board init moved to board.c

--- src/common/board.c
+++ src/common/board.c
@@ -3,3 +3,16 @@
 bool is_on_board(board_t board, unsigned int position) {
     return position >= 0 && position < (board.graph->num_vertices * board.graph->num_vertices);
 }
+
+board_t* init_board(struct graph_t* graph, unsigned int num_queens, unsigned int* queens[NUM_PLAYERS]) {
+    board_t* board = (board_t*) malloc(sizeof(board_t));
+    unsigned int** qu = queens;
+    board->graph = graph;
+    board->num_queens = num_queens;
+    board->queens = queens;
+    board->arrows = (bool*) malloc(sizeof(bool) * graph->num_vertices);
+    for (int i = 0; i < graph->num_vertices; ++i) {
+        board->arrows[i] = false;
+    }
+    return board;
+}
--- src/common/board.h
+++ src/common/board.h
@@ -10,10 +10,13 @@
 
 typedef struct {
     struct graph_t* graph;
-    unsigned int *queens[NUM_PLAYERS];
+    unsigned int** queens;
     unsigned int num_queens;
-    bool* arrows;
+    bool* arrows; // 1-d maping of the positions of the arrows
 } board_t;
+
+board_t* init_board(struct graph_t* graph, unsigned int num_queens, unsigned int* queens[NUM_PLAYERS]) {
+    board_t* board = (board_t*) malloc(sizeof(board_t));
 
 /// @brief return true if the given position is indexing a valid position on the board
 /// @param position the tested position
