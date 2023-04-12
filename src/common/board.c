4d293b3e0acf0c3a87e0e9e0d2ddc16fe66be467
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-04-12 15:33:30 +0200

    donut is done lol

--- src/common/board.c
+++ src/common/board.c
@@ -25,27 +25,34 @@ bool is_cell_empty(board_t *board, unsigned int cell_position)
             }
         }
     }
-  return !board->arrows[cell_position];
+    return !board->arrows[cell_position];
 }
 
-bool has_queen (unsigned int player_id, board_t *board, unsigned int queen_position) {
-    for (size_t i = 0; i < board->num_queens; ++i) {
-        if (board->queens[player_id][i] == queen_position) {
+bool has_queen(unsigned int player_id, board_t *board, unsigned int queen_position)
+{
+    for (size_t i = 0; i < board->num_queens; ++i)
+    {
+        if (board->queens[player_id][i] == queen_position)
+        {
             return true;
         }
     }
     return false;
 }
 // old, deprecated
-void add_reachable_positions_aligned_deprecated(board_t* board, position_set* reachable_positions, unsigned int neighbor, enum dir_t direction_to_neighbor, size_t width) {
+void add_reachable_positions_aligned_deprecated(board_t *board, position_set *reachable_positions, unsigned int neighbor, enum dir_t direction_to_neighbor, size_t width)
+{
     add_position(reachable_positions, neighbor);
     bool found = true;
-    while (found) {
+    while (found)
+    {
         found = false;
         size_t next_neighbor = 0;
-        while (next_neighbor < width) {
+        while (next_neighbor < width)
+        {
             enum dir_t direction_to_next_neighbor = gsl_spmatrix_uint_get(board->graph->t, neighbor, next_neighbor);
-            if (direction_to_next_neighbor == direction_to_neighbor && is_cell_empty(board, next_neighbor)) {
+            if (direction_to_next_neighbor == direction_to_neighbor && is_cell_empty(board, next_neighbor))
+            {
                 add_position(reachable_positions, next_neighbor);
                 neighbor = next_neighbor;
                 next_neighbor = 0;
@@ -58,27 +65,43 @@ void add_reachable_positions_aligned_deprecated(board_t* board, position_set* re
 }
 
 // Only works for square grids
-enum dir_t get_move_direction(unsigned int src, unsigned int dst, unsigned int width) {
-    if (dst > src) {
-        if (dst < src + width - src % width ) {
+enum dir_t get_move_direction(unsigned int src, unsigned int dst, unsigned int width)
+{
+    if (dst > src)
+    {
+        if (dst < src + width - src % width)
+        {
             return DIR_EAST;
         }
-        else if (dst % width == src % width) {
+        else if (dst % width == src % width)
+        {
             return DIR_SOUTH;
-        } else if (dst % (width + 1) == src % (width + 1)) {
+        }
+        else if (dst % (width + 1) == src % (width + 1))
+        {
             return DIR_SE;
-        } else if (dst % (width - 1) == src % (width - 1)) {
+        }
+        else if (dst % (width - 1) == src % (width - 1))
+        {
             return DIR_SW;
         }
-    } else if (dst < src) {
-        if (dst >= src - src % width ) {
+    }
+    else if (dst < src)
+    {
+        if (dst >= src - src % width)
+        {
             return DIR_WEST;
         }
-        else if (dst % width == src % width) {
+        else if (dst % width == src % width)
+        {
             return DIR_NORTH;
-        } else if (dst % (width + 1) == src % (width + 1)) {
+        }
+        else if (dst % (width + 1) == src % (width + 1))
+        {
             return DIR_NW;
-        } else if (dst % (width - 1) == src % (width - 1)) {
+        }
+        else if (dst % (width - 1) == src % (width - 1))
+        {
             return DIR_NE;
         }
     }
@@ -86,67 +109,74 @@ enum dir_t get_move_direction(unsigned int src, unsigned int dst, unsigned int w
 }
 
 // precond : src and dst are valid positions of the board. Board is a square grid.
-bool is_reachable_aligned_position(board_t *board, unsigned int src, unsigned int dst) {
+bool is_reachable_aligned_position(board_t *board, unsigned int src, unsigned int dst)
+{
     unsigned int width = sqrt(board->graph->num_vertices);
     int increment;
     size_t iterations;
     enum dir_t move_direction = get_move_direction(src, dst, width);
-    switch(move_direction) {
-        case DIR_EAST :
-            increment = 1;
-            iterations = width - src % width - 1;
-            break;
-        case DIR_NE :
-            increment = 1 - width;
-            iterations = width - src % width - 1;
-            break;
-        case DIR_SE :
-            increment = width + 1;
-            iterations = width - src % width - 1;
-            break;
-        case DIR_WEST :
-            increment = -1;
-            iterations = src % width;
-            break;
-        case DIR_NW :
-            increment = -width - 1;
-            iterations = src % width;
-            break;
-        case DIR_SW :
-            increment = width - 1;
-            iterations = src % width;
-            break;
-        case DIR_NORTH:
-            increment = -width;
-            iterations = (size_t) src / width ;
-            break;
-        case DIR_SOUTH:
-            increment = width;
-            iterations = (size_t) width - 1 - src / width ;
-            break;
-        default:
-            return false;
+    switch (move_direction)
+    {
+    case DIR_EAST:
+        increment = 1;
+        iterations = width - src % width - 1;
+        break;
+    case DIR_NE:
+        increment = 1 - width;
+        iterations = width - src % width - 1;
+        break;
+    case DIR_SE:
+        increment = width + 1;
+        iterations = width - src % width - 1;
+        break;
+    case DIR_WEST:
+        increment = -1;
+        iterations = src % width;
+        break;
+    case DIR_NW:
+        increment = -width - 1;
+        iterations = src % width;
+        break;
+    case DIR_SW:
+        increment = width - 1;
+        iterations = src % width;
+        break;
+    case DIR_NORTH:
+        increment = -width;
+        iterations = (size_t)src / width;
+        break;
+    case DIR_SOUTH:
+        increment = width;
+        iterations = (size_t)width - 1 - src / width;
+        break;
+    default:
+        return false;
     }
     unsigned int next_vertex = src;
-    for (size_t i = 0; i < iterations; ++i) {
+    for (size_t i = 0; i < iterations; ++i)
+    {
         next_vertex = next_vertex + increment;
-        if (gsl_spmatrix_uint_get(board->graph->t, src, next_vertex) != move_direction) {
+        if (gsl_spmatrix_uint_get(board->graph->t, src, next_vertex) != move_direction)
+        {
             return false;
-        } else if (next_vertex == dst) {
+        }
+        else if (next_vertex == dst)
+        {
             return true;
         }
     }
     return false;
 }
 
-bool is_move_legal(board_t *board, struct move_t *move, unsigned int player_id) {
+bool is_move_legal(board_t *board, struct move_t *move, unsigned int player_id)
+{
     return is_on_board(board, move->queen_src) &&
-            is_on_board(board, move->queen_dst) &&
-            is_on_board(board, move->arrow_dst) &&
-            is_cell_empty(board, move->queen_dst) &&
-            is_cell_empty(board, move->arrow_dst) &&
-            has_queen(player_id, board, move->queen_src) &&
-            is_reachable_aligned_position(board, move->queen_src, move->queen_dst);
+           is_on_board(board, move->queen_dst) &&
+           is_on_board(board, move->arrow_dst) &&
+           is_cell_empty(board, move->queen_dst) &&
+           is_cell_empty(board, move->arrow_dst) &&
+           has_queen(player_id, board, move->queen_src) &&
+           is_reachable_aligned_position(board, move->queen_src, move->queen_dst);
 }
 
 board_t *init_board(struct graph_t *graph, unsigned int num_queens)
@@ -187,15 +217,20 @@ void board_free(board_t *board)
     free(board);
 }
 
-void print_letter_line(unsigned int size){
-    for (unsigned int i = 0; i < size; ++i){
-        if (i > 0) printf(" ");
+void print_letter_line(unsigned int size)
+{
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        if (i > 0)
+            printf(" ");
         printf("%c", 'A' + i);
     }
 }
 
-void print_vertical_line(unsigned int size){
-    for (unsigned int i = 0; i < size; ++i){
+void print_vertical_line(unsigned int size)
+{
+    for (unsigned int i = 0; i < size; ++i)
+    {
         printf("-");
     }
 }
@@ -208,14 +243,35 @@ void print_board(board_t *board)
     printf("\n");
     printf("   /");
     print_vertical_line(width * 2 - 1);
-    printf("\\\n");
+    printf("-\\\n");
     for (unsigned int i = 0; i < width; ++i)
     {
         printf("%2d |", i + 1);
         for (unsigned int j = 0; j < width; ++j)
         {
-            if (j > 0) printf(" ");
+            if (j > 0)
+                printf(" ");
             unsigned int position = i * width + j;
+            bool isReachable = false;
+            for (unsigned int i = 0; i < board->graph->t->size1; i++)
+            {
+                for (unsigned int k = board->graph->t->p[i]; k < board->graph->t->p[i + 1]; k++)
+                {
+                    unsigned int j = board->graph->t->i[k];
+                    if (i == position || j == position)
+                    {
+                        isReachable = true;
+                        break;
+                    }
+                }
+                if (isReachable)
+                    break;
+            }
+            if (!isReachable)
+            {
+                printf(" ");
+                continue;
+            }
             if (board->arrows[position])
             {
                 printf("➴");
@@ -225,7 +281,7 @@ void print_board(board_t *board)
                 printf(RED "♛" RESET);
             }
             else if (has_queen(1, board, position))
-            {   
+            {
                 printf(CYAN "♛" RESET);
             }
             else
@@ -233,7 +289,7 @@ void print_board(board_t *board)
                 printf("▢");
             }
         }
-        printf("|\n");
+        printf(" |\n");
     }
     printf("   \\");
     print_vertical_line(width * 2 - 1);
--- src/server/game.c
+++ src/server/game.c
@@ -27,15 +27,16 @@ unsigned int get_other_player_id(unsigned int player_id)
 	return (player_id + 1) % NUM_PLAYERS;
 }
 
-void add_if_correct(size_t width, gsl_spmatrix_uint *mtrx, size_t x, size_t y, int dx, int dy){
-	if(dx == 0 && dy == 0)
+void add_if_correct(size_t width, gsl_spmatrix_uint *mtrx, size_t x, size_t y, int dx, int dy)
+{
+	if (dx == 0 && dy == 0)
 		return;
-	if((int)(x + dx) < 0 || x + dx >= width)
+	if ((int)(x + dx) < 0 || x + dx >= width)
 		return;
-	if((int)(y + dy) < 0 || y + dy >= width)
+	if ((int)(y + dy) < 0 || y + dy >= width)
 		return;
 	int i = x + y * width;
-	int j = (x+dx) + (y+dy)*width;
+	int j = (x + dx) + (y + dy) * width;
 	gsl_spmatrix_uint_set(mtrx, i, j, twoD_offset_to_dir(dx, dy));
 }
 
@@ -44,11 +45,14 @@ struct graph_t *init_square_graph(size_t width)
 	struct graph_t *graph = malloc(sizeof(struct graph_t));
 	graph->num_vertices = width * width;
 	gsl_spmatrix_uint *tmp = gsl_spmatrix_uint_alloc(graph->num_vertices, graph->num_vertices);
-	for(size_t i = 0; i<graph->num_vertices; i++){
+	for (size_t i = 0; i < graph->num_vertices; i++)
+	{
 		size_t x = i % width;
 		size_t y = i / width;
-		for(int dy = -1; dy <= 1; dy++){
-			for(int dx = -1; dx <= 1; dx++){
+		for (int dy = -1; dy <= 1; dy++)
+		{
+			for (int dx = -1; dx <= 1; dx++)
+			{
 				add_if_correct(width, tmp, x, y, dx, dy);
 			}
 		}
@@ -90,13 +94,61 @@ void init_queens(unsigned int **queens, unsigned int num_queens, size_t width)
 	}
 }
 
+bool pointInRectangle(size_t lx, size_t ly, size_t rx, size_t ry, size_t x, size_t y)
+{
+    if (x >= lx && x <= rx && y >= ly && y <= ry)
+        return true;
+ 
+    return false;
+}
+
+struct graph_t *init_donut_graph(size_t width)
+{
+	struct graph_t *graph = malloc(sizeof(struct graph_t));
+	graph->num_vertices = width * width;
+	size_t sq_offset = (width / 3) - 2;
+	size_t sq_size = (width / 3) - 1;
+	int offset = (int)(width / 6) - 1;
+	offset = offset < 0 ? 0 : offset;
+	size_t lx = (width / 2) - 1 - offset;
+	size_t ly = (width / 2) - 1 - offset;
+	size_t rx = lx + sq_size;
+	size_t ry = ly + sq_size;
+	gsl_spmatrix_uint *tmp = gsl_spmatrix_uint_alloc(graph->num_vertices, graph->num_vertices);
+	for (size_t i = 0; i < graph->num_vertices; i++)
+	{
+		size_t x = i % width;
+		size_t y = i / width;
+		if(pointInRectangle(lx, ly, rx, ry, x, y))
+			continue;
+		for (int dy = -1; dy <= 1; dy++)
+		{
+			for (int dx = -1; dx <= 1; dx++)
+			{
+				if(pointInRectangle(lx, ly, rx, ry, x + dx, y + dy))
+					continue;
+				add_if_correct(width, tmp, x, y, dx, dy);
+			}
+		}
+	}
+	graph->t = gsl_spmatrix_uint_compress(tmp, GSL_SPMATRIX_CSR);
+	gsl_spmatrix_uint_free(tmp);
+	return graph;
+}
+
 struct graph_t *init_graph(game_type_t game_type, size_t width)
 {
 	switch (game_type)
 	{
 	case SQUARE:
 		return init_square_graph(width);
-		break;
+	case DONUT:
+		if (width % 3 != 0)
+		{
+			fprintf(stderr, "For a DONUT graph, you need m mod 3 == 0\n");
+			exit(1);
+		}
+		return init_donut_graph(width);
 	default:
 		exit(1);
 	}
