1d91dc7a61ac82c3b17881643d9829364a00ee49
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-04-26 12:45:49 +0200

    made alphabeta actually finalize well

--- src/client/alphabeta.c
+++ src/client/alphabeta.c
@@ -13,7 +13,7 @@ void initialize(unsigned int player_id, struct graph_t *graph,
 {
 	generic_initialize(&global_player, player_id, graph, num_queens, queens, "alphabeta");
 	size_t width = (size_t)sqrt(graph->num_vertices);
-	divider = width + 0.7*width;
+	divider = width + 0.7 * width;
 }
 
 char const *get_player_name()
@@ -185,4 +185,6 @@ void finalize()
 {
 	board_free(global_player.board);
 	neighbors_cache_free();
+	turns = 0;
+	divider = 0;
 }
