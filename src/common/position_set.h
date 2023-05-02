380953f2f1b0ff50de5d1a3460068a3b31e6c5e0
Jean-Baptiste MARTINEZ <jeamartinez@enseirb-matmeca.fr>
2023-05-02 15:42:08 +0200

    common comments

--- src/common/position_set.h
+++ src/common/position_set.h
@@ -3,15 +3,33 @@
 
 #include <stdlib.h>
 
-typedef struct {
-    unsigned int* positions;
+typedef struct
+{
+    unsigned int *positions;
     size_t count;
 } position_set;
 
-position_set* init_position_set(size_t capacity);
-void add_position(position_set* set, unsigned int position);
+/// @brief initialize all the positions of the game
+/// @param capacity the number of maximm positions
+/// @return all the positions
+position_set *init_position_set(size_t capacity);
+
+/// @brief Add a position
+/// @param set the array of positions
+/// @param position the position
+void add_position(position_set *set, unsigned int position);
+
+/// @brief free all the positions
+/// @param position_set the positions
 void free_position_set(position_set *position_set);
-void print_position_set(position_set* set);
-void remove_position_at_index(position_set* set, size_t position_index);
+
+/// @brief print all the positions
+/// @param set the positions
+void print_position_set(position_set *set);
+
+/// @brief remove a position
+/// @param set all the positions
+/// @param position_index the index of the position removed
+void remove_position_at_index(position_set *set, size_t position_index);
 
 #endif // _POSITION_SET_H_
