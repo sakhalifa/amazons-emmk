6dae08bb3f97c380de653961cfbc09d655a5b72f
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-05-02 17:14:08 +0200

    test_is_cell_empty

--- test/test_board.c
+++ test/test_board.c
@@ -5,7 +5,8 @@
 #include <stdio.h>
 #include <assert.h>
 
-void free_board_without_queens(board_t *board){
+void free_board_without_queens(board_t *board)
+{
 	graph_free(board->graph);
 	free(board->arrows);
 	free(board->queens_on_board);
@@ -77,10 +78,61 @@ void test_is_on_board()
 	printf("OK\n");
 }
 
+void test_all_cell_empty_empty_board()
+{
+	board_t *board = init_board(init_square_graph(10), 0);
+	for (size_t pos = 0; pos < board->graph->num_vertices; pos++)
+	{
+		assert(is_cell_empty(board, pos));
+	}
+	free_board_without_queens(board);
+}
+
+void test_all_cell_not_empty_full_board()
+{
+	board_t *board = init_board(init_square_graph(10), 0);
+	for (size_t pos = 0; pos < board->graph->num_vertices; pos++)
+	{
+		board->arrows[pos] = true;
+	}
+
+	for (size_t pos = 0; pos < board->graph->num_vertices; pos++)
+	{
+		assert(!is_cell_empty(board, pos));
+	}
+	free_board_without_queens(board);
+}
+
+void test_queen_cells_not_empty()
+{
+	board_t *board = init_board(init_square_graph(10), 0);
+	unsigned int *queens[NUM_PLAYERS];
+	unsigned int queens0[4] = {0, 1, 2, 3};
+	unsigned int queens1[4] = {4, 5, 6, 7};
+	queens[0] = queens0;
+	queens[1] = queens1;
+	place_queens_on_board(board, queens, 4);
+	for (size_t pos = 0; pos < board->graph->num_vertices; pos++)
+	{
+		assert(is_cell_empty(board, pos) == !(pos < 8));
+	}
+	free_board_without_queens(board);
+}
+
+void test_is_cell_empty()
+{
+	printf("\t%s\t", __func__);
+	test_all_cell_empty_empty_board();
+	test_all_cell_not_empty_full_board();
+	test_queen_cells_not_empty();
+	printf("OK\n");
+}
+
 void run_board_tests()
 {
 	printf("Running test file \"%s\"\n", __FILE__);
 	test_is_on_board();
 	test_init_board();
 	test_place_queens_on_board();
+	test_is_cell_empty();
 }
